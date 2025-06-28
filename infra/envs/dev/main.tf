01e4d3d7b5b63a670ef1ce0471fc3c109719b009
theRealKarlos <karl.hitchcock@googlemail.com>
2025-06-28 01:23:58 +0100

    split bus add rules

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,23 +1,49 @@
-# Configure the AWS provider and region
+# Configure AWS provider
 provider "aws" {
   region = "eu-west-2"
 }
 
-# Deploy the order service Lambda function using the custom module
-module "order_service" {
-  source          = "../../modules/lambda/order-service"                      # Path to the Lambda module
-  lambda_zip_path = abspath("${path.module}/../../../dist/order-service.zip") # Path to the Lambda deployment package
-  function_name   = "order-service-handler"                                   # Name for the Lambda function
-  environment     = "dev"                                                     # Environment name
-  depends_on      = [module.eventbridge]                                      # Ensure EventBridge is created first
+# Create the custom EventBridge bus
+module "eventbridge_bus" {
+  source   = "../../modules/eventbridge/bus"
+  bus_name = "pulsequeue-bus"
 }
 
-# Deploy the custom EventBridge bus using the eventbridge module
-module "eventbridge" {
-  source   = "../../modules/eventbridge" # Path to the EventBridge module
-  bus_name = "pulsequeue-bus"            # Name for the EventBridge bus
+# Deploy the Lambda function using the custom module
+module "order_service" {
+  source          = "../../modules/lambda/order-service"
+  lambda_zip_path = abspath("${path.module}/../../../dist/order-service.zip")
+  function_name   = "order-service-handler"
+  environment     = "dev"
+
+  # Ensure the bus exists before Lambda tries to attach permissions
+  depends_on = [module.eventbridge_bus]
 }
-#de zip path
-#output "resolved_lambda_zip_path" {
-#  value = module.order_service.zip_path
-#}
+
+# Attach the OrderPlaced rule to the Lambda
+module "eventbridge_order_placed" {
+  source      = "../../modules/eventbridge/rule"
+  environment = "dev"
+  bus_name    = module.eventbridge_bus.bus_name
+  lambda_arn  = module.order_service.lambda_arn
+}
+
+output "lambda_function_name" {
+  value = module.order_service.function_name
+}
+
+output "lambda_function_arn" {
+  value = module.order_service.lambda_arn
+}
+
+output "eventbridge_bus_name" {
+  value = module.eventbridge_bus.bus_name
+}
+
+output "order_placed_rule_name" {
+  value = module.eventbridge_order_placed.rule_name
+}
+
+output "order_placed_rule_arn" {
+  value = module.eventbridge_order_placed.rule_arn
+}
