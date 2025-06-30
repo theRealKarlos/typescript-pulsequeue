d9a2d60cdf86f4f60b92bc063a309891835d0b77
theRealKarlos <karl.hitchcock@googlemail.com>
2025-06-30 16:48:11 +0100

    added comment

--- infra/modules/lambda/order-service/main.tf
+++ infra/modules/lambda/order-service/main.tf
@@ -9,5 +9,8 @@ resource "aws_lambda_function" "order_handler" {
   filename      = var.lambda_zip_path
   role          = aws_iam_role.lambda_exec.arn
 
+  # The source_code_hash is used to force updates to the Lambda function
+  # whenever the deployment package changes. Terraform will detect changes
+  # in the zip file and trigger a redeployment of the function.
   source_code_hash = filebase64sha256(var.lambda_zip_path)
 }
